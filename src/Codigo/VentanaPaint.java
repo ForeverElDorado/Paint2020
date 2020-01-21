61d868274eb29264901b4527df403fd3c38bd130
aghsk <aghsk@GLADOS>
2020-01-21 18:39:41 +0100

    Me falta el siguiente a palindromos 14/01/2020 Commit inicial del proyecto. Commit iniciando el ahorcado 2020. (15/01/2020 20:02) Commit sistema de botones del ahorcado hecho creo (15/01/2020 20:50) Commit imagenes de el ahorcado (16/01/2020 16:46) Commit todo acabado creo (17/01/2020 15:43) Commit hemos creado un pael en el paint que pinta muy bien (21/01/2020 16:53)

--- src/Codigo/VentanaPaint.java
+++ src/Codigo/VentanaPaint.java
@@ -61,6 +61,7 @@ public class VentanaPaint extends javax.swing.JFrame {
     private void initComponents() {
 
         Lienzo = new javax.swing.JPanel();
+        panelColores2 = new Codigo.PanelColores();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -87,14 +88,18 @@ public class VentanaPaint extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 161, Short.MAX_VALUE)
-                .addComponent(Lienzo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(Lienzo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelColores2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(Lienzo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelColores2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         pack();
@@ -102,7 +107,7 @@ public class VentanaPaint extends javax.swing.JFrame {
 
     private void LienzoMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_LienzoMouseDragged
         //metodo para el raton
-        bufferGraphics.setColor(Color.black);
+        bufferGraphics.setColor(panelColores2.colorSeleccionado);
         bufferGraphics.fillRect(evt.getX(), evt.getY(), 4, 4);
         //para repintar la pantalla
         repaint(0, 0, 1, 1);
@@ -146,5 +151,6 @@ public class VentanaPaint extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Lienzo;
+    private Codigo.PanelColores panelColores2;
     // End of variables declaration//GEN-END:variables
 }
